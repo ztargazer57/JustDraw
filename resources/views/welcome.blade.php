64357194ce698f736b87544d4a3311e2d71e024c
ztargazer57 <johnmhellsinagpulo@gmail.com>
2026-02-11 23:11:05 +0800

    Final

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="resources/css/extra.css" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-dark w-screen h-screen">
+    <!-- NAVBAR -->
+    <div class="bg-none shadow-xl h-25 p-5 flex">
+        <a class="btn btn-ghost text-4xl text-xdark font-bold text-shadow-4xl">JustDraw</a>
+        <div class="flex ml-auto gap-45">
+            <label class="input rounded-full w-200 h-15 text-xl bg-medium">
+                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                        stroke="currentColor">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input type="search" required placeholder="Search" />
+            </label>
+
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn m-1 rounded-full w-80 h-17 bg-medium border-0 p-2 -mt-1">
+                    <div class="avatar ml-auto">
+                        <div class="w-15 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                        </div>
+                    </div>
+                </div>
+                <ul tabindex="-1"
+                    class="dropdown-content menu bg-medium text-xl text-xlight rounded-box z-1 w-80 p-3 shadow-sm">
+                    <li><a>Item 1</a></li>
+                    <li><a>Item 2</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+        <!-- SIDEMENU -->
+        <div class="px-10 py-20">
+            <ul class="menu gap-20">
+                <li data-text="Home">
+                    <a class="tooltip tooltip-right p-2 bg-medium rounded-full" data-tip="Home">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                        </svg>
+                    </a>
+                </li>
+                <li data-text="Post">
+                    <a class="tooltip tooltip-right p-2 bg-medium rounded-full" data-tip="Details">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </a>
+                </li>
+                <li data-text="Gallery">
+                    <a class="tooltip tooltip-right p-2 bg-medium rounded-full" data-tip="Stats">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a class="tooltip tooltip-right p-2 bg-medium rounded-full" data-tip="Stats">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- MAIN CONTAINER -->
+</body>
+
+</html>
